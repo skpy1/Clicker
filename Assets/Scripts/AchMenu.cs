2cbbbc2d70df0a4182360cc4a3a9f7b680ccc3b5
skpy__ <101149710+skpy1@users.noreply.github.com>
2024-05-28 19:53:23 +0300

    ya seychas sdohny

--- Assets/Scripts/AchMenu.cs
+++ Assets/Scripts/AchMenu.cs
@@ -6,13 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class AchMenu : MonoBehaviour
 {
+    public int money;
     public int totalMoney;
     [SerializeField] Button firstAch;
     [SerializeField] bool isFirstAch;
+
+    [SerializeField] Button secondAch;
+    [SerializeField] bool isSecondAch;
+
+    [SerializeField] Button thirdAch;
+    [SerializeField] bool isThirdAch;
+
     void Start()
     {
+        money = PlayerPrefs.GetInt("money");
         totalMoney = PlayerPrefs.GetInt("totalMoney");
         isFirstAch = PlayerPrefs.GetInt("isFirstAch") == 1 ? true : false;
+        isSecondAch = PlayerPrefs.GetInt("isSecondAch") == 1 ? true : false;
+        isThirdAch = PlayerPrefs.GetInt("isThirdAch") == 1 ? true : false;
         if (totalMoney >= 100 && !isFirstAch)
         {
             firstAch.interactable = true;
@@ -20,20 +31,92 @@ public class AchMenu : MonoBehaviour
         else
         {
             firstAch.interactable = false;
+            StartCoroutine(IdleFarm());
+        }
+        if (totalMoney >= 1000 && !isSecondAch)
+        {
+            secondAch.interactable = true;
+        }
+        else
+        {
+            secondAch.interactable = false;
+        }
+        if (totalMoney >= 10000 && !isThirdAch)
+        {
+            thirdAch.interactable = true;
+        }
+        else
+        {
+            thirdAch.interactable = false;
         }
     }
 
-    public void GetFirst()
+    public void GetFirst(int n)
     {
         int money = PlayerPrefs.GetInt("money");
-        money += 100;
+        money += n;
+        Debug.Log(money);
+        PlayerPrefs.SetInt("money", money);
+        Debug.Log(PlayerPrefs.GetInt("money"));
+        int totalMoney = PlayerPrefs.GetInt("totalMoney");
+        totalMoney += n;
+        PlayerPrefs.SetInt("totalMoney", totalMoney);
+        if (n == 100){
+            isFirstAch = true;
+            PlayerPrefs.SetInt("isFirstAch", 1);
+            firstAch.interactable = false;
+            PlayerPrefs.Save();
+        }
+        else if (n == 1000)
+        {
+            isSecondAch = true;
+            PlayerPrefs.SetInt("isSecondAch", 1);
+            secondAch.interactable = false;
+            PlayerPrefs.Save();
+        }
+        else if (n == 10000)
+        {
+            isThirdAch = true;
+            PlayerPrefs.SetInt("isThirdAch", 1);
+            thirdAch.interactable = false;
+            PlayerPrefs.Save();
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void GetSecond()
+    {
+        int money = PlayerPrefs.GetInt("money");
+        money += 1000;
         PlayerPrefs.SetInt("money", money);
         int totalMoney = PlayerPrefs.GetInt("totalMoney");
-        totalMoney += 100;
+        totalMoney += 1000;
         PlayerPrefs.SetInt("totalMoney", totalMoney);
-        isFirstAch = true;
-        PlayerPrefs.SetInt("isFirstAch", isFirstAch ? 1 : 0);
-        firstAch.interactable = false;
+        isSecondAch = true;
+        PlayerPrefs.SetInt("isSecondAch", 1);
+        secondAch.interactable = false;
+        PlayerPrefs.Save();
+    }
+
+    public void GetThird()
+    {
+        int money = PlayerPrefs.GetInt("money");
+        money += 10000;
+        PlayerPrefs.SetInt("money", money);
+        int totalMoney = PlayerPrefs.GetInt("totalMoney");
+        totalMoney += 10000;
+        PlayerPrefs.SetInt("totalMoney", totalMoney);
+        isThirdAch = true;
+        PlayerPrefs.SetInt("isThirdAch", 1);
+        thirdAch.interactable = false;
+    }
+
+    IEnumerator IdleFarm()
+    {
+        yield return new WaitForSeconds(1);
+        money++;
+        PlayerPrefs.SetInt("money", money);
+        StartCoroutine(IdleFarm());
     }
 
     public void ToMenu()
@@ -41,8 +124,4 @@ public class AchMenu : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -12,22 +13,72 @@ public class MainMenu : MonoBehaviour
     [SerializeField] int money;
     public int totalMoney;
     public Text moneyText;
-   
+    public GameObject effect;
+    public GameObject button;
+    public AudioSource audioSource;
+
     private void Start()
     {
-        moneyText.text = "0";
         //PlayerPrefs.DeleteAll();
+        audioSource = GetComponent<AudioSource>();
         money = PlayerPrefs.GetInt("money");
         totalMoney = PlayerPrefs.GetInt("totalMoney");
+        Debug.Log(money);
+
+        bool isFirst = PlayerPrefs.GetInt("isFirst") == 1 ? true : false;
+        StartCoroutine(IdleFarm());
+        //OfflineTime();
     }
 
     public void ButtonClick()
     {
-        money++;
-        totalMoney++;
+        money += 10;
+        totalMoney += 10;
         PlayerPrefs.SetInt("money", money);
         PlayerPrefs.SetInt("totalMoney", totalMoney);
+        Instantiate(effect, button.GetComponent<RectTransform>().position.normalized, Quaternion.identity);
+        button.GetComponent<RectTransform>().localScale = new Vector3(0.95f, 0.95f, 0f);
+        audioSource.Play();
     }
+
+    public void OnClickUp()
+    {
+        button.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 0f);
+    }
+
+    IEnumerator IdleFarm()
+    {
+        yield return new WaitForSeconds(1);
+        money++;
+        PlayerPrefs.SetInt("money", money);
+        StartCoroutine(IdleFarm());
+    }
+
+    private void OfflineTime()
+    {
+        TimeSpan ts;
+        if (PlayerPrefs.HasKey("LastSession"))
+        {
+            ts = DateTime.Now - DateTime.Parse(PlayerPrefs.GetString("LastSession"));
+            money += (int)ts.TotalSeconds;
+        }
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            PlayerPrefs.SetString("LastSession", DateTime.Now.ToString());
+        }
+    }
+#else
+    private void OnApplicationQuit()
+    {
+            PlayerPrefs.SetString("LastSession", DateTime.Now.ToString());
+    }
+#endif
 
     public void ToAchievements()
     {
